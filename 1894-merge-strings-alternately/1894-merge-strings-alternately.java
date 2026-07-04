0f4553e0ac7f7c83bf75c8299a553613098e2be6
Shashu-02 <shashanktp2006@gmail.com>
2026-07-04 23:19:22 +0530

    1894-MERGE-STRINGS-ALTERNATELY Stats: Time: 1 ms (96.01%), Space: 43.2 MB (48.55%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,22 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String res="";
+        StringBuilder res=new StringBuilder();
         int i=0;
         int j=0;
         while(i<word1.length()&&j<word2.length()){
-            res=res+word1.charAt(i);
+            res.append(word1.charAt(i));
             i++;
-            res=res+word2.charAt(j);
+            res.append(word2.charAt(j));
             j++;
         }
         while(i<word1.length()){
-            res=res+word1.charAt(i);
+            res.append(word1.charAt(i));
             i++;
         }
         while(j<word2.length()){
-            res=res+word2.charAt(j);
+            res.append(word2.charAt(j));
             j++;
         }
-        return res;
+        return res.toString();
     }
 }
