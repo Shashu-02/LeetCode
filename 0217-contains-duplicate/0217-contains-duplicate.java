9c86d49b46b105a0904517216b6e5b0dcd1d330f
Shashu-02 <shashanktp2006@gmail.com>
2026-07-08 22:02:15 +0530

    0217-CONTAINS-DUPLICATE Stats: Time: 24 ms (24.72%), Space: 92.7 MB (74.8%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,12 +2,8 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set=new HashSet<>();
         for(int x:nums){
-            if(set.contains(x)){
-                return true;
-            }else{
-                set.add(x);
-            }
+            set.add(x);
         }
-        return false;
+        return set.size()==nums.length?false:true;
     }
 }
