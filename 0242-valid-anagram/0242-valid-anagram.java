d99ceea93622bdb06470e0aa405fd7626e4d0675
Shashu-02 <shashanktp2006@gmail.com>
2026-09-09 21:32:31 +0530

    0242-VALID-ANAGRAM Stats: Time: 16 ms (21.21%), Space: 46.7 MB (16.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         HashMap<Character,Integer> ms=new HashMap<>();
-        HashMap<Character,Integer> mt=new HashMap<>();
         for(char x:s.toCharArray()){
             ms.put(x,ms.getOrDefault(x,0)+1);
         }
         for(char x:t.toCharArray()){
-            mt.put(x,mt.getOrDefault(x,0)+1);
+            if(ms.containsKey(x)){
+                ms.put(x,ms.get(x)-1);
+            }else{
+                return false;
+            }
+            if(ms.get(x)==0){
+                ms.remove(x);
+            }
         }
-        return ms.equals(mt);     
+        if(ms.isEmpty()) return true;
+        return false;
     }
 }
